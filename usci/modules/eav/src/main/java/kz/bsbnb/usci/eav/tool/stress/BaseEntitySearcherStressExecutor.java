9a8ed81b141649890f9f62a00d8694f5e22374a5
Artur Tkachenko <429076@gmail.com>
2013-03-06 15:14:55 +0600

    - BaseEntityDao minor fixes

--- usci/modules/eav/src/main/java/kz/bsbnb/usci/eav/tool/stress/BaseEntitySearcherStressExecutor.java
+++ usci/modules/eav/src/main/java/kz/bsbnb/usci/eav/tool/stress/BaseEntitySearcherStressExecutor.java
@@ -30,7 +30,7 @@ public class BaseEntitySearcherStressExecutor
         BasicBaseEntitySearcher bes = new BasicBaseEntitySearcher();
         System.out.println("Test started at: " + Calendar.getInstance().getTime());
 
-        MetaClassGenerator metaClassGenerator = new MetaClassGenerator(25, 2);
+        MetaClassGenerator metaClassGenerator = new MetaClassGenerator(25, 2, 2);
         BaseEntityGenerator baseEntityGenerator = new BaseEntityGenerator();
 
         ClassPathXmlApplicationContext ctx
@@ -59,7 +59,7 @@ public class BaseEntitySearcherStressExecutor
 
             for(int i = 0; i < dataSize; i++)
             {
-                MetaClass metaClass = metaClassGenerator.generateMetaClass(0);
+                MetaClass metaClass = metaClassGenerator.generateMetaClass();
 
                 long metaClassId = metaClassDao.save(metaClass);
 
