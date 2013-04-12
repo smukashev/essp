96e2103a4c56f3ad521f1c09c30e450b4695174d
kanat90@gmail.com <kanat90@gmail.com>
2013-04-12 17:59:33 +0600

    Unnecessary constant deleted

--- usci/modules/receiver/src/main/java/kz/bsbnb/usci/receiver/common/Global.java
+++ usci/modules/receiver/src/main/java/kz/bsbnb/usci/receiver/common/Global.java
@@ -11,7 +11,6 @@ public class Global {
     public static final String BATCH_STATUS_ERROR = "ERROR";
     public static final String BATCH_STATUS_COMPLETED = "COMPLETED";
 
-    public static final String CONTRACT_STATUS_STARTED = "STARTED";
     public static final String CONTRACT_STATUS_PROCESSING = "PROCESSING";
     public static final String CONTRACT_STATUS_ERROR = "ERROR";
     public static final String CONTRACT_STATUS_COMPLETED = "COMPLETED";
