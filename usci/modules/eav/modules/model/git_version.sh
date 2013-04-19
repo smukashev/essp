b370c28b201e952e7b2f55db1f9092ecc4747b69
Artur Tkachenko <429076@gmail.com>
2013-04-19 13:45:38 +0600

    - Some minor changes

--- usci/modules/eav/modules/model/git_version.sh
+++ usci/modules/eav/modules/model/git_version.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-branch=`git branch|grep \*|cut -d" " -f 2`
-commit=`git id`
+mkdir -p "$1"
 
-echo "buildNumber=${branch}.${commit}" > "$1"
+branch=`git branch|grep \*|cut -d" " -f 2`
+commit=`git describe`
+
+echo "buildNumber=${branch}.${commit}" > "$1/$2"
