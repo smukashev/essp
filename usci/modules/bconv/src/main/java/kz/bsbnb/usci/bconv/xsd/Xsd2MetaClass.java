126fb7f79500b0d2d0d2232fb1545c05745d57b8
Artur Tkachenko <429076@gmail.com>
2013-07-31 17:51:44 +0600

    - MetaClass creation added to XSD parser

--- usci/modules/bconv/src/main/java/kz/bsbnb/usci/bconv/xsd/Xsd2MetaClass.java
+++ usci/modules/bconv/src/main/java/kz/bsbnb/usci/bconv/xsd/Xsd2MetaClass.java
@@ -2,13 +2,20 @@ package kz.bsbnb.usci.bconv.xsd;
 
 import com.sun.xml.xsom.*;
 import com.sun.xml.xsom.parser.XSOMParser;
+import kz.bsbnb.usci.eav.model.meta.IMetaType;
+import kz.bsbnb.usci.eav.model.meta.impl.MetaAttribute;
 import kz.bsbnb.usci.eav.model.meta.impl.MetaClass;
+import kz.bsbnb.usci.eav.model.meta.impl.MetaSet;
+import kz.bsbnb.usci.eav.model.meta.impl.MetaValue;
+import kz.bsbnb.usci.eav.model.type.DataTypes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Vector;
 
 public class Xsd2MetaClass
 {
@@ -18,7 +25,6 @@ public class Xsd2MetaClass
 
     public static MetaClass convertXSD(InputStream schema, String metaClassName)
     {
-        MetaClass meta = new MetaClass();
         XSSchema xsSchema;
 
         if (schema == null) {
@@ -50,21 +56,37 @@ public class Xsd2MetaClass
             throw new IllegalArgumentException("No such class: " + metaClassName);
         }
 
-        processComplexType("ct_package", ct, 1, "");
-
-        return meta;
+        return (MetaClass)processComplexType("ct_package", ct, 1, "");
     }
 
-    private static void processComplexType(String name, XSComplexType ct, int maxOccurs, String prefix)
+    private static IMetaType processComplexType(String name, XSComplexType ct, int maxOccurs, String prefix)
     {
         String attributeStr = "";
         int attrCount = 0;
+        HashMap<String, IMetaType> members = new HashMap<String, IMetaType>();
+        boolean itIsArray = false;
 
         try {
             boolean first = true;
             attrCount = ct.getAttributeUses().size();
             for (XSAttributeUse attributes : ct.getAttributeUses()) {
                 //System.out.println(prefix + "Attribute - " + attributes.getDecl().getName());
+
+                if(attributes.getDecl().getType().getPrimitiveType().getName().equals("string"))
+                    members.put(attributes.getDecl().getName(), new MetaValue(DataTypes.STRING));
+                else if(attributes.getDecl().getType().getPrimitiveType().getName().equals("date"))
+                    members.put(attributes.getDecl().getName(), new MetaValue(DataTypes.DATE));
+                else if(attributes.getDecl().getType().getPrimitiveType().getName().equals("decimal"))
+                    members.put(attributes.getDecl().getName(), new MetaValue(DataTypes.DOUBLE));
+                else if(attributes.getDecl().getType().getPrimitiveType().getName().equals("boolean"))
+                    members.put(attributes.getDecl().getName(), new MetaValue(DataTypes.BOOLEAN));
+                else if(attributes.getDecl().getType().getPrimitiveType().getName().equals("integer"))
+                    members.put(attributes.getDecl().getName(), new MetaValue(DataTypes.INTEGER));
+                else
+                    throw new IllegalStateException("Can't convert \"" +
+                            attributes.getDecl().getType().getPrimitiveType().getName() + "\" to " +
+                            "MetaValue: unknown simple type");
+
                 if(first) {
                     attributeStr += attributes.getDecl().getName() + " : " + attributes.getDecl().getType().
                             getPrimitiveType().getName();
@@ -127,33 +149,62 @@ public class Xsd2MetaClass
                         System.out.println(prefix + name + "[" + maxOccurs + "]" +
                                 comppType + subtypesStr + attributeStr + " {");
                     }
+                    itIsArray = true;
                 }
             }
 
             //System.out.println("## " + ct.getBaseType());
             if (!ct.getBaseType().getName().equals("anyType") && ct.getBaseType().isSimpleType())
-                processType(ct.getBaseType().getName(), ct.getBaseType(), maxOccurs, prefix + offset);
+            {
+                IMetaType mt = processType(ct.getBaseType().getName(), ct.getBaseType(), maxOccurs, prefix + offset);
+                members.put(ct.getBaseType().getName(), mt);
+            }
 
             if (particles != null) {
-                processModelGroupContents(particles, prefix + offset);
+                processModelGroupContents(members, particles, prefix + offset);
             }
 
             if ((particlesCount + attrCount) > 1 ||
                     (!ct.getBaseType().getName().equals("anyType") && ct.getBaseType().isSimpleType()) ||
-                    noFlatten)
+                    noFlatten) {
                 System.out.println(prefix + "}");
+            }
         } catch (Exception ex) {
             System.out.println(prefix + "Error in complex type contents!!!");
         }
+
+        if (members.size() > 1) {
+            MetaClass metaClass = new MetaClass(name);
+
+            for(String memberName : members.keySet()) {
+                MetaAttribute attr = new MetaAttribute(members.get(memberName));
+                metaClass.setMetaAttribute(memberName, attr);
+            }
+
+            if (!itIsArray)
+                return metaClass;
+            else
+                return new MetaSet(metaClass);
+        } else {
+            if (members.size() < 1)
+                throw new IllegalStateException("MetaClass with no members: " + name);
+            if (itIsArray) {
+                return new MetaSet(members.get(members.keySet().toArray()[0]));
+            } else {
+                return members.get(members.keySet().toArray()[0]);
+            }
+        }
     }
 
-    private static void processModelGroupContents(XSParticle[] particles, String prefix)
+    private static void processModelGroupContents(HashMap<String, IMetaType> members,
+                                                  XSParticle[] particles, String prefix)
     {
         for(XSParticle p : particles ){
             XSTerm pterm = p.getTerm();
             if(pterm.isElementDecl()){ //xs:element inside complex type
                 //System.out.println(prefix + "Element - " + pterm.asElementDecl().getName());
-                processElement(pterm, p.getMaxOccurs(), prefix);
+                IMetaType mt = processElement(pterm, p.getMaxOccurs(), prefix);
+                members.put(pterm.asElementDecl().getName(), mt);
             }
             if(pterm.isModelGroup()){ //xs:element inside complex type
                 if(p.getMaxOccurs() != 1 || pterm.asModelGroup().getCompositor() == XSModelGroup.Compositor.CHOICE) {
@@ -178,47 +229,77 @@ public class Xsd2MetaClass
                 {
                     if(p.getMaxOccurs() != 1 ||
                             pterm.asModelGroup().getCompositor() == XSModelGroup.Compositor.CHOICE) {
-                        processModelGroupContents(pp, prefix + "  ");
+                        processModelGroupContents(members, pp, prefix + "  ");
                         System.out.println(prefix + "]");
                     } else {
-                        processModelGroupContents(pp, prefix);
+                        processModelGroupContents(members, pp, prefix);
                     }
                 }
             }
         }
     }
 
-    private static void processSimpleType(String name, XSSimpleType ct, int maxOccurs, String prefix)
+    private static IMetaType processSimpleType(String name, XSSimpleType ct, int maxOccurs, String prefix)
     {
         if (maxOccurs == 1) {
             System.out.println(prefix + name + " : " + ct.getPrimitiveType().getName());
+
+            if(ct.getPrimitiveType().getName().equals("string"))
+                return new MetaValue(DataTypes.STRING);
+            else if(ct.getPrimitiveType().getName().equals("date"))
+                return new MetaValue(DataTypes.DATE);
+            else if(ct.getPrimitiveType().getName().equals("decimal"))
+                return new MetaValue(DataTypes.DOUBLE);
+            else if(ct.getPrimitiveType().getName().equals("boolean"))
+                return new MetaValue(DataTypes.BOOLEAN);
+            else if(ct.getPrimitiveType().getName().equals("integer"))
+                return new MetaValue(DataTypes.INTEGER);
+            else
+                throw new IllegalStateException("Can't convert \"" + ct.getPrimitiveType().getName() + "\" to " +
+                    "MetaValue: unknown simple type");
         } else {
             if (maxOccurs == -1) {
                 System.out.println(prefix + name + "[] : " + ct.getPrimitiveType().getName());
             } else {
                 System.out.println(prefix + name + "[" + maxOccurs + "] : " + ct.getPrimitiveType().getName());
             }
+
+            if(ct.getPrimitiveType().getName().equals("string"))
+                return new MetaSet(new MetaValue(DataTypes.STRING));
+            else if(ct.getPrimitiveType().getName().equals("date"))
+                return new MetaSet(new MetaValue(DataTypes.DATE));
+            else if(ct.getPrimitiveType().getName().equals("decimal"))
+                return new MetaSet(new MetaValue(DataTypes.DOUBLE));
+            else if(ct.getPrimitiveType().getName().equals("boolean"))
+                return new MetaSet(new MetaValue(DataTypes.BOOLEAN));
+            else if(ct.getPrimitiveType().getName().equals("integer"))
+                return new MetaSet(new MetaValue(DataTypes.INTEGER));
+            else
+                throw new IllegalStateException("Can't convert \"" + ct.getPrimitiveType().getName() + "\" to " +
+                        "MetaValue: unknown simple type");
         }
     }
 
-    private static void processElement(XSTerm el, int maxOccurs, String prefix)
+    private static IMetaType processElement(XSTerm el, int maxOccurs, String prefix)
     {
         XSType type = el.asElementDecl().getType();
 
-        processType(el.asElementDecl().getName(), type, maxOccurs, prefix);
+        return processType(el.asElementDecl().getName(), type, maxOccurs, prefix);
     }
 
-    private static void processType(String name, XSType type, int maxOccurs, String prefix)
+    private static IMetaType processType(String name, XSType type, int maxOccurs, String prefix)
     {
         if (type.isComplexType())
         {
-            processComplexType(name, type.asComplexType(), maxOccurs, prefix);
+            return processComplexType(name, type.asComplexType(), maxOccurs, prefix);
         }
 
         if (type.isSimpleType())
         {
-            processSimpleType(name, type.asSimpleType(), maxOccurs, prefix);
+            return processSimpleType(name, type.asSimpleType(), maxOccurs, prefix);
         }
+
+        throw new IllegalStateException("Type is not complex or simple.");
     }
 
     private static XSParticle[] getParticles(XSParticle particle)
