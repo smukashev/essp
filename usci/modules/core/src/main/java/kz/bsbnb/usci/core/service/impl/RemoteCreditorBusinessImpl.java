885ccb32a23fe67bf669a3a3fc1320df784ba14d
Artur Tkachenko <429076@gmail.com>
2013-10-02 17:54:43 +0600

    - Added support classes for admin portlet

--- usci/modules/core/src/main/java/kz/bsbnb/usci/core/service/impl/RemoteCreditorBusinessImpl.java
+++ usci/modules/core/src/main/java/kz/bsbnb/usci/core/service/impl/RemoteCreditorBusinessImpl.java
@@ -2,6 +2,11 @@ package kz.bsbnb.usci.core.service.impl;
 
 import kz.bsbnb.usci.core.service.RemoteCreditorBusiness;
 import kz.bsbnb.usci.cr.model.Creditor;
+import kz.bsbnb.usci.eav.model.base.impl.BaseEntity;
+import kz.bsbnb.usci.eav.model.base.impl.BaseValue;
+import kz.bsbnb.usci.eav.persistance.dao.IBaseEntityDao;
+import kz.bsbnb.usci.eav.repository.IMetaClassRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -10,20 +15,37 @@ import java.util.List;
 @Service
 public class RemoteCreditorBusinessImpl implements RemoteCreditorBusiness
 {
+    @Autowired
+    IBaseEntityDao baseEntityDao;
+
+    @Autowired
+    IMetaClassRepository metaClassRepository;
+
     @Override
     public List<Creditor> findMainOfficeCreditors()
     {
-        Creditor creditor = new Creditor();
+        List<BaseEntity> entities = baseEntityDao.getEntityByMetaclass(
+                metaClassRepository.getMetaClass("ref_creditor"));
 
-        creditor.setId(1);
-        creditor.setName("Creditor1");
-        creditor.setShortName("C1");
-        creditor.setCode("CODE1");
+        ArrayList<Creditor> creditors = new ArrayList<Creditor>();
 
-        ArrayList<Creditor> list = new ArrayList<Creditor>();
+        for (BaseEntity entity : entities) {
+            Creditor creditor = new Creditor();
 
-        list.add(creditor);
+            creditor.setId(1);
+            BaseValue value = (BaseValue)entity.getBaseValue("name");
+            if (value != null)
+                creditor.setName((String)value.getValue());
 
-        return list;
+            value = (BaseValue)entity.getBaseValue("short_name");
+            if (value != null)
+                creditor.setShortName((String)value.getValue());
+
+            value = (BaseValue)entity.getBaseValue("code");
+            if (value != null)
+                creditor.setCode((String)value.getValue());
+        }
+
+        return creditors;
     }
 }
