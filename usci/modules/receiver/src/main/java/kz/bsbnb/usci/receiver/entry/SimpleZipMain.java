9c494af8aae4f1ccfa3868d497b2aea50bb9cbda
atkachenko <429076@gmail.com>
2014-03-20 17:05:29 +0600

    Merged with master

--- usci/modules/receiver/src/main/java/kz/bsbnb/usci/receiver/entry/SimpleZipMain.java
+++ usci/modules/receiver/src/main/java/kz/bsbnb/usci/receiver/entry/SimpleZipMain.java
@@ -33,8 +33,8 @@ public class SimpleZipMain {
         StatusSingleton statusSingleton = ctx.getBean(StatusSingleton.class);
 
         ZipFilesMonitor monitor = ctx.getBean(ZipFilesMonitor.class);
-        //monitor.monitor(Paths.get("c:\\zips"));
-        monitor.monitor(Paths.get("/home/a.tkachenko/input"));
+        monitor.monitor(Paths.get("c:\\zips"));
+        //monitor.monitor(Paths.get("/home/a.tkachenko/input"));
 
     }
 }
