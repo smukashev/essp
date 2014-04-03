b2de660b7d53b1e7a4cc6d3df40d51334b8ad87f
atkachenko <429076@gmail.com>
2014-04-03 13:11:50 +0600

    - File appender configuration added to log4j in core, sync, receiver

--- usci/couchbase_views/views.js
+++ usci/couchbase_views/views.js
@@ -1,51 +1,42 @@
-//http://blog.couchbase.com/understanding-grouplevel-view-queries-compound-keys
-//batch
-
-function (doc, meta) {
-   if(doc.type == "batch") {
+{
+   "views":{
+      "batch":{
+         "map":"function (doc, meta) {
+   if(doc.type == \"batch\") {
      emit(meta.id.substring(6), {id: doc.id, fileName: doc.fileName, received: doc.received});
    }
-   if(doc.type == "batch_status") {
+   if(doc.type == \"batch_status\") {
      emit(meta.id.substring(13), {batchStatuses: doc.batchStatuses});
    }
- }
-
-function(key, values, rereduce) {
-          var obj = values[1];
-          obj["status"] = values[0];
-
-          return obj;
-        }
-
-//batch_pending
-
-function (doc, meta) {
-  if(doc.type == "batch_status" && doc.batchStatuses[doc.batchStatuses.length - 1].protocol != "COMPLETED") {
-    emit(meta.id.substring(13), doc.batchStatuses[doc.batchStatuses.length - 1].protocol);
-  }
-}
-
-//batch_sign
-
-function (doc, meta) {
-              if(doc.type == "sign" && !(doc.hasOwnProperty("sign"))) {
-                emit(doc.userId, doc);
-              }
-            }
-
-//contract_status
-//use key filter - [batch_id, index]
-
-function (doc, meta) {
-  if(doc.type == "contract_status") {
-    emit([doc.batchId, doc.index], {index: doc.index, contractStatuses: doc.contractStatuses});
-  }
-}
-
-//batch_creditor
-function (doc, meta) {
-   if(doc.type == "batch") {
+ }"
+      },
+      "batch_creditor":{
+         "map":"function (doc, meta) {
+   if(doc.type == \"batch\") {
      emit(doc.id, doc.creditorId);
    }
- }
-
+ }"
+      },
+      "batch_pending":{
+         "map":"function (doc, meta) {
+  if(doc.type == \"batch_status\" && doc.batchStatuses[doc.batchStatuses.length - 1].protocol != \"COMPLETED\") {
+    emit(parseInt(meta.id.substring(13)), doc.batchStatuses[doc.batchStatuses.length - 1].protocol);
+  }
+}"
+      },
+      "batch_sign":{
+         "map":"function (doc, meta) {
+              if(doc.type == \"sign\" && !(doc.hasOwnProperty(\"sign\"))) {
+                emit(doc.userId, doc);
+              }
+            }"
+      },
+      "contract_status":{
+         "map":"function (doc, meta) {
+  if(doc.type == \"contract_status\") {
+    emit([doc.batchId, doc.index], {index: doc.index, contractStatuses: doc.contractStatuses});
+  }
+}"
+      }
+   }
+}
