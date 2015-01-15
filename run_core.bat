5cd96e33a68541c3545d7043d557d4f66026f626
Kanat Tulbassiyev <kanat90@gmail.com>
2015-01-15 16:36:15 +0600

    bat files fixed

--- run_core.bat
+++ run_core.bat
@@ -1,5 +1,3 @@
-set JAVA_BIN_PATH="c:\Progra~1\Java\jdk1.7.0\bin\"
-
-%JAVA_BIN_PATH%java -Djava.security.policy=./distr/no.policy -Dspring.profiles.active=oracle -jar ./distr/core-0.0.1-SNAPSHOT.one-jar.jar
-
+java -cp "usci\modules\core\target\dependency\*;usci\modules\core\target\core-0.0.1-SNAPSHOT.jar" -Djava.security.policy=no.policy kz.bsbnb.usci.brms.rulesvr.Main
+ 
 pause
