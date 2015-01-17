fa07f3971f76929d27f84bc00d58d84664ea69c5
Kanat Tulbassiyev <kanat90@gmail.com>
2015-01-17 17:47:36 +0600

    *.bat files fixed

--- run_cli.bat
+++ run_cli.bat
@@ -1,3 +1,3 @@
-java -cp "usci\modules\cli\target\dependency\*;usci\modules\cli\target\cli-0.0.1-SNAPSHOT.jar" -Djava.security.policy=no.policy kz.bsbnb.usci.brms.rulesvr.Main
+java -cp "usci\modules\cli\target\dependency\*;usci\modules\cli\target\cli-0.0.1-SNAPSHOT.jar" -Djava.security.policy=no.policy kz.bsbnb.usci.cli.CLIMain
  
 pause
--- run_receiver.bat
+++ run_receiver.bat
@@ -1,3 +1,3 @@
-java -cp "usci\modules\receiver\target\dependency\*;usci\modules\receiver\target\receiver-0.0.1-SNAPSHOT.jar" -Djava.security.policy=no.policy kz.bsbnb.usci.brms.rulesvr.Main
+java -cp "usci\modules\receiver\target\dependency\*;usci\modules\receiver\target\receiver-0.0.1-SNAPSHOT.jar" -Djava.security.policy=no.policy kz.bsbnb.usci.receiver.SimpleZipMain
  
 pause
