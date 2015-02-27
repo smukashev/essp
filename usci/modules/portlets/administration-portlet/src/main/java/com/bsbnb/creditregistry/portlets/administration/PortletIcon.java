5c1b3de7697d2e50c86822a0bb0888a1451b5bd9
Kanat Tulbassiyev <kanat90@gmail.com>
2015-02-27 13:50:47 +0600

    Credit-registry-administration-portlet has been renamed to USCI Portlets: AdministrationPortlet

--- usci/modules/portlets/administration-portlet/src/main/java/com/bsbnb/creditregistry/portlets/administration/PortletIcon.java
+++ usci/modules/portlets/administration-portlet/src/main/java/com/bsbnb/creditregistry/portlets/administration/PortletIcon.java
@@ -6,7 +6,7 @@ import com.vaadin.terminal.ExternalResource;
  * @author Marat Madybayev
  */
 public final class PortletIcon extends ExternalResource {
-    public static final String CONTEXT_NAME = "credit-registry-administration-portlet";
+    public static final String CONTEXT_NAME = "administration-portlet";
 
     public static final PortletIcon DOWN_ICON = new PortletIcon("icons/down_1.png");
     public static final PortletIcon UP_ICON = new PortletIcon("icons/up_1.png");
