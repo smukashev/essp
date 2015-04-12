1699c4bd4b1c2dd62414749db07ebdd96ba7485e
kanat90@gmail.com <_Kane123>
2015-04-12 15:56:27 +0600

    Incorrect access types were removed

--- usci/modules/eav/modules/persistance/src/main/java/kz/bsbnb/usci/eav/persistance/dao/IBaseEntityProcessorDao.java
+++ usci/modules/eav/modules/persistance/src/main/java/kz/bsbnb/usci/eav/persistance/dao/IBaseEntityProcessorDao.java
@@ -1,10 +1,8 @@
 package kz.bsbnb.usci.eav.persistance.dao;
 
-import kz.bsbnb.usci.eav.manager.IBaseEntityManager;
 import kz.bsbnb.usci.eav.manager.IBaseEntityMergeManager;
 import kz.bsbnb.usci.eav.model.RefListItem;
 import kz.bsbnb.usci.eav.model.base.IBaseEntity;
-import kz.bsbnb.usci.eav.model.base.IBaseSet;
 import kz.bsbnb.usci.eav.model.base.impl.BaseEntity;
 import kz.bsbnb.usci.eav.model.meta.IMetaClass;
 import kz.bsbnb.usci.eav.model.meta.impl.MetaClass;
@@ -33,59 +31,59 @@ public interface IBaseEntityProcessorDao
      * @return obtained instance of the BaseEntity by the search.
      * @since 1.0
      */
-    public long search(IBaseEntity baseEntity);
+    long search(IBaseEntity baseEntity);
 
-    public List<Long> search(long metaClassId);
+    List<Long> search(long metaClassId);
 
-    public List<Long> search(String className);
+    List<Long> search(String className);
 
-    public IBaseEntity loadByMaxReportDate(long id, Date reportDate);
+    IBaseEntity loadByMaxReportDate(long id, Date reportDate);
 
-    public IBaseEntity loadByMaxReportDate(long id, Date reportDate, boolean caching);
+    IBaseEntity loadByMaxReportDate(long id, Date reportDate, boolean caching);
 
-    public IBaseEntity loadByMinReportDate(long id, Date reportDate);
+    IBaseEntity loadByMinReportDate(long id, Date reportDate);
 
-    public IBaseEntity loadByMinReportDate(long id, Date reportDate, boolean caching);
+    IBaseEntity loadByMinReportDate(long id, Date reportDate, boolean caching);
 
-    public IBaseEntity loadByReportDate(long id, Date actualReportDate, boolean caching);
+    IBaseEntity loadByReportDate(long id, Date actualReportDate, boolean caching);
 
-    public IBaseEntity loadByReportDate(long id, Date actualReportDate);
+    IBaseEntity loadByReportDate(long id, Date actualReportDate);
 
-    public IBaseEntity load(long id);
+    IBaseEntity load(long id);
 
-    public IBaseEntity load(long id, boolean caching);
+    IBaseEntity load(long id, boolean caching);
 
-    public IBaseEntity load(long id, Date maxReportDate, Date actualReportDate);
+    IBaseEntity load(long id, Date maxReportDate, Date actualReportDate);
 
-    public IBaseEntity load(long id, Date maxReportDate, Date actualReportDate, boolean caching);
+    IBaseEntity load(long id, Date maxReportDate, Date actualReportDate, boolean caching);
 
-    public IBaseEntity prepare(IBaseEntity baseEntity);
+    IBaseEntity prepare(IBaseEntity baseEntity);
 
-    public IBaseEntity process(IBaseEntity baseEntity);
+    IBaseEntity process(IBaseEntity baseEntity);
 
-    public List<Long> getEntityIDsByMetaclass(long metaClassId);
+    List<Long> getEntityIDsByMetaclass(long metaClassId);
 
-    public List<RefListItem> getRefsByMetaclass(long metaClassId);
+    List<RefListItem> getRefsByMetaclass(long metaClassId);
 
-    public List<RefListItem> getRefsByMetaclassRaw(long metaClassId);
+    List<RefListItem> getRefsByMetaclassRaw(long metaClassId);
 
-    public List<BaseEntity> getEntityByMetaclass(MetaClass meta);
+    List<BaseEntity> getEntityByMetaclass(MetaClass meta);
 
-    public boolean isApproved(long id);
+    boolean isApproved(long id);
 
-    public int batchCount(long id, String className);
+    int batchCount(long id, String className);
 
-    public boolean remove(long baseEntityId);
+    boolean remove(long baseEntityId);
 
-    public long getRandomBaseEntityId(long metaClassId);
+    long getRandomBaseEntityId(long metaClassId);
 
-    public long getRandomBaseEntityId(IMetaClass metaClass);
+    long getRandomBaseEntityId(IMetaClass metaClass);
 
-    public Set<Long> getChildBaseEntityIds(long parentBaseEntityIds);
+    Set<Long> getChildBaseEntityIds(long parentBaseEntityIds);
 
-    public IDaoListener getApplyListener();
+    IDaoListener getApplyListener();
 
-    public void setApplyListener(IDaoListener applyListener);
+    void setApplyListener(IDaoListener applyListener);
 
     List<Date> getEntityReportDates(Long entityId);
 
@@ -95,29 +93,29 @@ public interface IBaseEntityProcessorDao
         LEFT
     }
 
-    public IBaseEntity merge(IBaseEntity baseEntityLeft, IBaseEntity baseEntityRight,
+    IBaseEntity merge(IBaseEntity baseEntityLeft, IBaseEntity baseEntityRight,
                              IBaseEntityMergeManager mergeManager, MergeResultChoice choice);
 
-    public void populate(String metaName, Long id, Date reportDate);
+    void populate(String metaName, Long id, Date reportDate);
 
     void populateSC(Long creditorId);
 
     void populateSC();
 
-    public List<Long> getSCEntityIds(Long id);
+    List<Long> getSCEntityIds(Long id);
 
-    public List<Long[]> getSCEntityIds(int limit, Long prevMaxId);
+    List<Long[]> getSCEntityIds(int limit, Long prevMaxId);
 
-    public void removeSCEntityIds(List<Long> list, Long id);
+    void removeSCEntityIds(List<Long> list, Long id);
 
     void removeSCEntityIds(List<Long> entityIds);
 
-    public void removeShowcaseId(Long id);
+    void removeShowcaseId(Long id);
 
-    public List<Long> getShowcaseIdsToLoad();
+    List<Long> getShowcaseIdsToLoad();
 
-    public List<Long> getNewTableIds(Long id);
+    List<Long> getNewTableIds(Long id);
 
-    public void removeNewTableIds(List<Long> list, Long id);
+    void removeNewTableIds(List<Long> list, Long id);
 
 }
