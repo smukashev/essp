650d438eb1ee9d9fd05976c3326a78eae8ad021a
Bauyrzhan Makhambetov <baurmatic@gmail.com>
2015-04-27 20:43:44 +0600

    null bug in ref retrieval

--- usci/modules/eav/modules/persistance/src/main/java/kz/bsbnb/usci/eav/persistance/dao/impl/BaseEntityProcessorDaoImpl.java
+++ usci/modules/eav/modules/persistance/src/main/java/kz/bsbnb/usci/eav/persistance/dao/impl/BaseEntityProcessorDaoImpl.java
@@ -3110,59 +3110,66 @@ public class BaseEntityProcessorDaoImpl extends JDBCSupport implements IBaseEnti
         List<Map<String, Object>> rows = queryForListWithStats(select.getSQL(), select.getBindValues().toArray());
 
         Iterator<Map<String, Object>> i = rows.iterator();
-        while (i.hasNext()) {
-            RefListItem rli = new RefListItem();
+        RefListItem rli = null;
+        long prevId = -1;
 
+        while (i.hasNext()) {
             Map<String, Object> row = i.next();
             long id = ((BigDecimal) row.get("ID")).longValue();
-            long old_id = id;
+
+            if(prevId != id) {
+                if(rli != null)
+                    entityIds.add(rli);
+                rli = new RefListItem();
+            }
+            prevId = id;
 
             logger.debug("#####################");
 
             rli.setId(id);
-            while (old_id == id) {
-                if (((String) row.get("NAME")).equals("code")) {
-                    String code = (String) row.get("VALUE");
-                    if(!raw)
-                        code = Quote.addSlashes(code);
-                    rli.setCode(code);
-                } else if (((String) row.get("NAME")).startsWith("name_")) {
-                    String value = (String) row.get("VALUE");
-                    if(!raw)
-                        value = Quote.addSlashes(value);
-                    rli.setTitle(value);
-                } else if (((String) row.get("NAME")).startsWith("name")) {
-                    String value = (String) row.get("VALUE");
-                    if(!raw)
-                        value = Quote.addSlashes(value);
-                    rli.setTitle(value);
-                }
 
-                for (String key : row.keySet()) {
-                    if (key.equals("NAME") || key.startsWith("name_")) {
-                        continue;
-                    }
-
-                    String value = row.get(key).toString();
-
-                    if(!raw)
-                        value = Quote.addSlashes(value);
-
-                    //System.out.println("###% " + value);
-
-                    rli.addValue(key, value);
-                }
-
-                if (!i.hasNext())
-                    break;
-
-                row = i.next();
-                old_id = id;
-                id = ((BigDecimal) row.get("ID")).longValue();
+            if (((String) row.get("NAME")).equals("code")) {
+                String code = (String) row.get("VALUE");
+                if(!raw)
+                    code = Quote.addSlashes(code);
+                rli.setCode(code);
+            } else if (((String) row.get("NAME")).startsWith("name_")) {
+                String value = (String) row.get("VALUE");
+                if(!raw)
+                    value = Quote.addSlashes(value);
+                rli.setTitle(value);
+            } else if (((String) row.get("NAME")).startsWith("name")) {
+                String value = (String) row.get("VALUE");
+                if(!raw)
+                    value = Quote.addSlashes(value);
+                rli.setTitle(value);
             }
 
-            entityIds.add(rli);
+            for (String key : row.keySet()) {
+                if (key.equals("NAME") || key.startsWith("name_")) {
+                    continue;
+                }
+
+                String value = row.get(key).toString();
+
+                if(!raw)
+                    value = Quote.addSlashes(value);
+
+                //System.out.println("###% " + value);
+
+                rli.addValue(key, value);
+            }
+
+            //if (!i.hasNext())
+            //    break;
+
+            //row = i.next();
+            //old_id = id;
+            //id = ((BigDecimal) row.get("ID")).longValue();
         }
+
+        if(rli!=null)
+            entityIds.add(rli);
 
         if(refsCacheEnalbed)
             refsCache.put(metaClassId, entityIds);
