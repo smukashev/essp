da442253c0de2aded9d7d980faa9d5b6c87cc225
ktulbassiyev <kanat90@gmail.com>
2015-06-05 15:40:33 +0600

    exceptionOnImmutableWrite modified to true

--- usci/modules/eav/modules/persistance/src/main/java/kz/bsbnb/usci/eav/tool/CommonConfig.java
+++ usci/modules/eav/modules/persistance/src/main/java/kz/bsbnb/usci/eav/tool/CommonConfig.java
@@ -3,5 +3,5 @@ package kz.bsbnb.usci.eav.tool;
 public class CommonConfig {
     public final static boolean throwExceptionOnMultipleReportDate = true;
 
-    public static final boolean exceptionOnImmutableWrite = false;
+    public static final boolean exceptionOnImmutableWrite = true;
 }
