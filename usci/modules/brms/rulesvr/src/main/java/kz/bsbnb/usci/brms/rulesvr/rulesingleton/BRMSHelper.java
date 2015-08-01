4409f7dd084d654c935a0bf284da3790e9edd76c
Бауыржан Махамбетов <baurmatic@gmail.com>
2015-08-01 20:29:46 +0600

    1) currency short_name and code fix. 2) Rule: currency rule representation

--- usci/modules/brms/rulesvr/src/main/java/kz/bsbnb/usci/brms/rulesvr/rulesingleton/BRMSHelper.java
+++ usci/modules/brms/rulesvr/src/main/java/kz/bsbnb/usci/brms/rulesvr/rulesingleton/BRMSHelper.java
@@ -84,27 +84,6 @@ public class BRMSHelper
 
     }
 
-    public static boolean isCurrencyConvertible(String currencyCode, Date reportDate){
-
-        String select = "select value from (\n" +
-                "select sconv.value,\n" +
-                "       rank() over(order by sconv.report_date desc) as ra\n" +
-                "  from eav_be_boolean_values sconv\n" +
-                " where sconv.report_date <= ? \n" +
-                "   and sconv.is_closed = 0\n" +
-                "   and sconv.entity_id = (select scode.entity_id\n" +
-                "                            from eav_be_string_values scode\n" +
-                "                           where scode.attribute_id = (select atr.id from vw_simple_attribute atr \n" +
-                "                                                         where atr.class_name = 'ref_currency'\n" +
-                "                                                         and atr.attribute_name = 'code')\n" +
-                "                             and scode.value = ?)\n" +
-                ") where ra  = 1";
-
-        int ans = jdbcTemplate.queryForInt(select, new Object[]{reportDate, currencyCode});
-
-        return ans > 0;
-    }
-
     public static boolean isValidRNN(String rnn)
     {
         String chr;
