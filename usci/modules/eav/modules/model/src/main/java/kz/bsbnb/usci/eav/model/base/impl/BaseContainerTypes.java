e6da95e4f1309de089599a26b35a9fbec951efb4
ktulbassiyev <kanat90@gmail.com>
2015-10-12 14:31:43 +0600

    OperationType values become final

--- usci/modules/eav/modules/model/src/main/java/kz/bsbnb/usci/eav/model/base/impl/BaseContainerTypes.java
+++ usci/modules/eav/modules/model/src/main/java/kz/bsbnb/usci/eav/model/base/impl/BaseContainerTypes.java
@@ -4,7 +4,6 @@ package kz.bsbnb.usci.eav.model.base.impl;
  * @author alexandr.motov
  */
 public class BaseContainerTypes {
-
     public static final int BASE_ENTITY = 1;
     public static final int BASE_SET = 2;
 
--- usci/modules/eav/modules/model/src/main/java/kz/bsbnb/usci/eav/model/base/impl/OperationTypes.java
+++ usci/modules/eav/modules/model/src/main/java/kz/bsbnb/usci/eav/model/base/impl/OperationTypes.java
@@ -1,9 +1,9 @@
 package kz.bsbnb.usci.eav.model.base.impl;
 
 public class OperationTypes {
-    public static int DELETE = 1;
-    public static int NEW = 2;
-    public static int CLOSE = 3;
-    public static int INSERT = 4;
-    public static int UPDATE = 5;
+    public static final int DELETE = 1;
+    public static final int NEW = 2;
+    public static final int CLOSE = 3;
+    public static final int INSERT = 4;
+    public static final int UPDATE = 5;
 }
