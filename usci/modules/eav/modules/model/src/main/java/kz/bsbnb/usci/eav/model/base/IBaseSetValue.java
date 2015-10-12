7a6bded7c2683845cdb55677c1dd2fb76544046b
ktulbassiyev <kanat90@gmail.com>
2015-10-12 14:24:38 +0600

    RESTRICTION removed

--- usci/modules/eav/modules/model/src/main/java/kz/bsbnb/usci/eav/model/base/IBaseSetValue.java
+++ usci/modules/eav/modules/model/src/main/java/kz/bsbnb/usci/eav/model/base/IBaseSetValue.java
@@ -4,11 +4,4 @@ package kz.bsbnb.usci.eav.model.base;
  * @author alexandr.motov
  */
 public interface IBaseSetValue<T> extends IBaseValue<T> {
-
-    public enum HistoryType { RESTRICTED_BY_SET, RESTRICTED_BY_ENTITY, INHERITED }
-
-    public HistoryType getHistoryType();
-
-    public void setHistoryType(HistoryType historyType);
-
 }
