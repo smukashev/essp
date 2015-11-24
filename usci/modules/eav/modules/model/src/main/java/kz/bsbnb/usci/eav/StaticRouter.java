83b1a85baed7a8518eb0c38747f782e7a62f4599
kanat90 <kanat90@gmail.com>
2015-11-24 15:29:31 +0600

    comments added to StaticRouter, reporter schema IP added to StaticRouter

--- usci/modules/eav/modules/model/src/main/java/kz/bsbnb/usci/eav/StaticRouter.java
+++ usci/modules/eav/modules/model/src/main/java/kz/bsbnb/usci/eav/StaticRouter.java
@@ -6,20 +6,30 @@ public final class StaticRouter {
         PROD
     }
 
+    /* Set up before compiling */
     private static final MODE mode = MODE.PROD;
 
+    /* Application Server IP */
     private final static String devAsIP = "localhost";
     private final static String prodAsIP = "10.8.1.116";
 
+    /* Core Schema IP */
     private final static String devDBCoreIP = "localhost";
     private final static String prodDBCoreIp = "10.8.1.200";
 
+    /* Showcase Schema IP */
     private final static String devDBShowcaseIP = "localhost";
     private final static String prodDBShowcaseIP = "10.8.1.85";
 
+    /* Reporter Schema IP */
+    private final static String devDBReporterIP = "localhost";
+    private final static String prodDBReporterIP = "10.8.1.85";
+
+    /* Portal URL */
     private final static String devPortalUrl = "localhost";
     private final static String prodPortalUrl = "192.168.20.50";
 
+    /* Portal port */
     private final static String devPortalPort = "80";
     private final static String prodPortalPort = "80";
 
@@ -51,6 +61,17 @@ public final class StaticRouter {
                 return devDBShowcaseIP;
             case PROD:
                 return prodDBShowcaseIP;
+            default:
+                throw new IllegalStateException("Неизвестный мод;");
+        }
+    }
+
+    public static String getDBReporterIP() {
+        switch(mode) {
+            case DEV:
+                return devDBReporterIP;
+            case PROD:
+                return prodDBReporterIP;
             default:
                 throw new IllegalStateException("Неизвестный мод;");
         }
