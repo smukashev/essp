090016439b3024fc16d3d3891ac1c6ecbdf9998f
Kanat Tulbassiyev <kanat90@gmail.com>
2015-11-28 14:13:34 +0600

    ojdbc6 driver version changed to 11.2.0.4

--- usci/libs/exec.sh
+++ usci/libs/exec.sh
@@ -1,3 +1,3 @@
 mvn install:install-file -Dfile=easyuploads-0.5.1.jar -DgroupId=org.vaadin.addons -DartifactId=easyuploads -Dversion=0.5.1 -Dpackaging=jar
 
-mvn install:install-file -Dfile=ojdbc6.jar -DgroupId=com.oracle -DartifactId=ojdbc6 -Dversion=11.2.0.3.0 -Dpackaging=jar
+mvn install:install-file -Dfile=ojdbc6.jar -DgroupId=com.oracle -DartifactId=ojdbc6 -Dversion=11.2.0.4 -Dpackaging=jar
