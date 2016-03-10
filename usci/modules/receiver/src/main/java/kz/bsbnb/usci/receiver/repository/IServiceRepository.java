14dc6749928f5ab70d76d446e24b02867ba1d753
Kanat Tulbassiyev <kanat.tulbassiyev@bsbnb.kz>
2016-03-10 08:42:10 +0600

    refactored

--- usci/modules/receiver/src/main/java/kz/bsbnb/usci/receiver/repository/IServiceRepository.java
+++ usci/modules/receiver/src/main/java/kz/bsbnb/usci/receiver/repository/IServiceRepository.java
@@ -4,7 +4,6 @@ import kz.bsbnb.usci.core.service.IGlobalService;
 import kz.bsbnb.usci.core.service.MailMessageBeanCommonBusiness;
 import kz.bsbnb.usci.core.service.PortalUserBeanRemoteBusiness;
 import kz.bsbnb.usci.core.service.RemoteCreditorBusiness;
-import kz.bsbnb.usci.cr.model.PortalUser;
 import kz.bsbnb.usci.sync.service.IBatchService;
 import kz.bsbnb.usci.sync.service.IEntityService;
 import kz.bsbnb.usci.sync.service.IMetaFactoryService;
@@ -15,11 +14,18 @@ import kz.bsbnb.usci.sync.service.ReportBeanRemoteBusiness;
  */
 public interface IServiceRepository {
     IEntityService getEntityService();
+
     IBatchService getBatchService();
+
     IMetaFactoryService getMetaFactoryService();
+
     PortalUserBeanRemoteBusiness getUserService();
+
     RemoteCreditorBusiness getRemoteCreditorBusiness();
+
     ReportBeanRemoteBusiness getReportBeanRemoteBusinessService();
+
     MailMessageBeanCommonBusiness getMailMessageBeanCommonBusiness();
+
     IGlobalService getGlobalService();
 }
