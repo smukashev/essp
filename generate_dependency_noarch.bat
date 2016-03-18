131c5fd1b05a4b5887314c23830c04b497ebef78
Bauyrzhan Makhambetov <baurmatic@gmail.com>
2016-03-18 17:09:00 +0600

    mvn script update nb

--- generate_dependency_noarch.bat
+++ generate_dependency_noarch.bat
@@ -1,6 +1,6 @@
-mvn dependency:copy-dependencies -f usci/modules/brms/rulesvr/pom.xml
-mvn dependency:copy-dependencies -f usci/modules/core/pom.xml
-mvn dependency:copy-dependencies -f usci/modules/sync/pom.xml
-mvn dependency:copy-dependencies -f usci/modules/showcase/pom.xml
-mvn dependency:copy-dependencies -f usci/modules/receiver/pom.xml
-mvn dependency:copy-dependencies -f usci/modules/cli/pom.xml
+mvn -s D:\DevTools\apache-maven-3.2.5\conf\settings.xml dependency:copy-dependencies -f usci/modules/brms/rulesvr/pom.xml
+mvn -s D:\DevTools\apache-maven-3.2.5\conf\settings.xml dependency:copy-dependencies -f usci/modules/core/pom.xml
+mvn -s D:\DevTools\apache-maven-3.2.5\conf\settings.xml dependency:copy-dependencies -f usci/modules/sync/pom.xml
+mvn -s D:\DevTools\apache-maven-3.2.5\conf\settings.xml dependency:copy-dependencies -f usci/modules/showcase/pom.xml
+mvn -s D:\DevTools\apache-maven-3.2.5\conf\settings.xml dependency:copy-dependencies -f usci/modules/receiver/pom.xml
+mvn -s D:\DevTools\apache-maven-3.2.5\conf\settings.xml dependency:copy-dependencies -f usci/modules/cli/pom.xml
