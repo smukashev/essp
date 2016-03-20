3698a224abedd06a09a6156d6a0e9db24df9a227
Kanat Tulbassiyev <kanat.tulbassiyev@bsbnb.kz>
2016-03-20 20:42:33 +0600

    runs Xms & Xmx installed

--- run_cli.sh
+++ run_cli.sh
@@ -1,1 +1,1 @@
-java -Djava.security.policy=no.policy -cp "usci/modules/cli/target/cli-0.0.1-SNAPSHOT.jar:usci/modules/cli/target/dependency/*" kz.bsbnb.usci.cli.CLIMain $1
+java -Xms1024m -Xmx4096m -Djava.security.policy=no.policy -cp "usci/modules/cli/target/cli-0.0.1-SNAPSHOT.jar:usci/modules/cli/target/dependency/*" kz.bsbnb.usci.cli.CLIMain $1
