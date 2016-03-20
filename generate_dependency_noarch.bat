06521662386b0c0c736d2f9041cc14fbf328af0e
Kanat Tulbassiyev <kanat.tulbassiyev@bsbnb.kz>
2016-03-20 20:20:40 +0600

    generate_dependecy_noarch.bat fixed with multiple mvn commands

--- generate_dependency_noarch.bat
+++ generate_dependency_noarch.bat
@@ -1,6 +1,6 @@
-mvn -s D:\DevTools\apache-maven-3.2.5\conf\settings.xml dependency:copy-dependencies -f usci/modules/brms/rulesvr/pom.xml
-mvn -s D:\DevTools\apache-maven-3.2.5\conf\settings.xml dependency:copy-dependencies -f usci/modules/core/pom.xml
-mvn -s D:\DevTools\apache-maven-3.2.5\conf\settings.xml dependency:copy-dependencies -f usci/modules/sync/pom.xml
-mvn -s D:\DevTools\apache-maven-3.2.5\conf\settings.xml dependency:copy-dependencies -f usci/modules/showcase/pom.xml
-mvn -s D:\DevTools\apache-maven-3.2.5\conf\settings.xml dependency:copy-dependencies -f usci/modules/receiver/pom.xml
-mvn -s D:\DevTools\apache-maven-3.2.5\conf\settings.xml dependency:copy-dependencies -f usci/modules/cli/pom.xml
+call mvn -s D:\DevTools\apache-maven-3.2.5\conf\settings.xml dependency:copy-dependencies -f usci/modules/brms/rulesvr/pom.xml
+call mvn -s D:\DevTools\apache-maven-3.2.5\conf\settings.xml dependency:copy-dependencies -f usci/modules/core/pom.xml
+call mvn -s D:\DevTools\apache-maven-3.2.5\conf\settings.xml dependency:copy-dependencies -f usci/modules/sync/pom.xml
+call mvn -s D:\DevTools\apache-maven-3.2.5\conf\settings.xml dependency:copy-dependencies -f usci/modules/showcase/pom.xml
+call mvn -s D:\DevTools\apache-maven-3.2.5\conf\settings.xml dependency:copy-dependencies -f usci/modules/receiver/pom.xml
+call mvn -s D:\DevTools\apache-maven-3.2.5\conf\settings.xml dependency:copy-dependencies -f usci/modules/cli/pom.xml
