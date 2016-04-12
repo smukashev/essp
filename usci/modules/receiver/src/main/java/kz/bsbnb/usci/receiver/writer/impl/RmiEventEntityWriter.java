cf71ac84fd2b633c078e79fd16e933dc98d41fb1
Kanat Tulbassiyev <kanat.tulbassiyev@bsbnb.kz>
2016-04-12 18:01:01 +0600

    RmiEventEntityWriter modified

--- usci/modules/receiver/src/main/java/kz/bsbnb/usci/receiver/writer/impl/RmiEventEntityWriter.java
+++ usci/modules/receiver/src/main/java/kz/bsbnb/usci/receiver/writer/impl/RmiEventEntityWriter.java
@@ -1,16 +1,7 @@
 package kz.bsbnb.usci.receiver.writer.impl;
 
-import kz.bsbnb.usci.brms.rulemodel.service.IRuleService;
-import kz.bsbnb.usci.eav.model.EntityStatus;
-import kz.bsbnb.usci.eav.model.base.impl.BaseEntity;
-import kz.bsbnb.usci.eav.model.base.impl.OperationType;
 import kz.bsbnb.usci.eav.stats.SQLQueriesStats;
-import kz.bsbnb.usci.eav.util.EntityStatuses;
-import kz.bsbnb.usci.eav.util.Errors;
 import kz.bsbnb.usci.receiver.common.Global;
-import kz.bsbnb.usci.receiver.repository.IServiceRepository;
-import kz.bsbnb.usci.sync.service.IBatchService;
-import kz.bsbnb.usci.tool.status.StatusProperties;
 import kz.bsbnb.usci.receiver.writer.IWriter;
 import kz.bsbnb.usci.sync.service.IEntityService;
 import org.apache.log4j.Logger;
@@ -21,7 +12,7 @@ import org.springframework.remoting.rmi.RmiProxyFactoryBean;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.util.*;
+import java.util.List;
 
 /**
  * @author k.tulbassiyev
@@ -29,46 +20,16 @@ import java.util.*;
 @Component
 @Scope("step")
 public class RmiEventEntityWriter<T> implements IWriter<T> {
-    public static final String LOGIC_RULE_SETTING = "LOGIC_RULE_SETTING";
-    public static final String LOGIC_RULE_META = "LOGIC_RULE_META";
-
     @SuppressWarnings("SpringJavaAutowiringInspection")
     @Autowired
     @Qualifier(value = "remoteEntityService")
     private RmiProxyFactoryBean rmiProxyFactoryBean;
 
-    @SuppressWarnings("SpringJavaAutowiringInspection")
-    @Autowired
-    @Qualifier(value="remoteRuleService")
-    private RmiProxyFactoryBean rmiProxyRuleService;
-
-    @Autowired
-    private IServiceRepository serviceFactory;
-
     private IEntityService entityService;
-    private Logger logger = Logger.getLogger(RmiEventEntityWriter.class);
-    private IRuleService ruleService;
-
-    private IBatchService batchService;
-
-    @Autowired
-    protected SQLQueriesStats sqlStats;
-
-    @Autowired
-    protected Global global;
-
-    private Set<String> metaRules = new HashSet<>();
 
     @PostConstruct
     public void init() {
-        logger.info("Writer init");
-
         entityService = (IEntityService) rmiProxyFactoryBean.getObject();
-        ruleService = (IRuleService) rmiProxyRuleService.getObject();
-        batchService = serviceFactory.getBatchService();
-
-        String[] metas = serviceFactory.getGlobalService().getValue(LOGIC_RULE_SETTING, LOGIC_RULE_META).split(",");
-        Collections.addAll(metaRules, metas);
     }
 
     @Override
