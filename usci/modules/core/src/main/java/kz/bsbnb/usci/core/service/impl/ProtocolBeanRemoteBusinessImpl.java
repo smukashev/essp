0db3f1c1d80745b48929f3b9de61bef7a405d50f
Askhat Anetbayev <askhat.anetbayev@gmail.com>
2016-04-15 16:29:07 +0600

    FIX Protocol portlet:  Общее количество, Заявленное количество

--- usci/modules/core/src/main/java/kz/bsbnb/usci/core/service/impl/ProtocolBeanRemoteBusinessImpl.java
+++ usci/modules/core/src/main/java/kz/bsbnb/usci/core/service/impl/ProtocolBeanRemoteBusinessImpl.java
@@ -6,16 +6,18 @@ import kz.bsbnb.usci.cr.model.InputInfo;
 import kz.bsbnb.usci.cr.model.Message;
 import kz.bsbnb.usci.cr.model.Protocol;
 import kz.bsbnb.usci.cr.model.Shared;
-import kz.bsbnb.usci.eav.util.Errors;
+import kz.bsbnb.usci.eav.model.Batch;
 import kz.bsbnb.usci.eav.model.EntityStatus;
 import kz.bsbnb.usci.eav.util.EntityStatuses;
-import org.apache.commons.lang.StringUtils;
+import kz.bsbnb.usci.eav.util.Errors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static kz.bsbnb.usci.eav.util.EntityStatuses.*;
 
@@ -37,16 +39,18 @@ public class ProtocolBeanRemoteBusinessImpl implements ProtocolBeanRemoteBusines
 
         List<EntityStatus> entityStatusList = batchService.getEntityStatusList(batchId);
 
+        Batch batch = batchService.getBatch(batchId);
+
         for (EntityStatus entityStatus : entityStatusList) {
             if (protocolsToDisplay.contains(entityStatus.getStatus())) {
-                fillProtocol(entityStatus, inputInfoId, list);
+                fillProtocol(entityStatus, inputInfoId, list, batch);
             }
         }
 
         return list;
     }
 
-    private void fillProtocol(EntityStatus entityStatus, InputInfo inputInfoId, ArrayList<Protocol> list) {
+    private void fillProtocol(EntityStatus entityStatus, InputInfo inputInfoId, ArrayList<Protocol> list, Batch batch) {
 
         Protocol protocol = new Protocol();
         protocol.setId(1L);
@@ -93,17 +97,17 @@ public class ProtocolBeanRemoteBusinessImpl implements ProtocolBeanRemoteBusines
         type.setNameKz(entityStatus.getStatusDescription());
 
         protocol.setMessage(message);
-        protocol.setMessageType(type);
         protocol.setProtocolType(type);
+        protocol.setMessageType(type);
 
         if (TOTAL_COUNT == entityStatus.getStatus()) {
             message.setNameRu("Общее количество:");
             message.setNameKz("Общее количество:");
-            protocol.setNote(entityStatus.getDescription());
+            protocol.setNote("" + batch.getTotalCount());
         } else if (ACTUAL_COUNT == entityStatus.getStatus()) {
             message.setNameRu("Заявленное количество:");
             message.setNameKz("Заявленное количество:");
-            protocol.setNote(entityStatus.getDescription());
+            protocol.setNote("" + batch.getActualCount());
         } else {
             if (COMPLETED == entityStatus.getStatus()) {
                 message.setNameRu("Идентификатор сущности:");
@@ -111,7 +115,7 @@ public class ProtocolBeanRemoteBusinessImpl implements ProtocolBeanRemoteBusines
                 protocol.setNote(entityStatus.getEntityId() + "");
             }
 
-            protocol.setTypeDescription(entityStatus.getDescription());
+            protocol.setTypeDescription(entityStatus.getContractNumber());
         }
 
         list.add(protocol);
