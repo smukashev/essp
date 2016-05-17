c044f00764ff26e6f5c23b3a37b62589797ae857
Askhat Anetbayev <askhat.anetbayev@gmail.com>
2016-05-17 11:41:52 +0600

    logback -> log4j

--- usci/modules/manager/src/main/java/kz/bsbnb/usci/manager/App.java
+++ usci/modules/manager/src/main/java/kz/bsbnb/usci/manager/App.java
@@ -1,6 +1,8 @@
 package kz.bsbnb.usci.manager;
 
 import kz.bsbnb.usci.manager.db.TaskDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -9,10 +11,12 @@ import java.util.TimerTask;
  * Created by askhat.anetbayev@gmail.com
  */
 public class App {
+	final static Logger logger = LoggerFactory.getLogger(App.class);
 	public static void main(String[] args) {
 		TimerTask task = new TaskDAO();
 
 		Timer timer = new Timer();
 		timer.schedule(task, 1000, 60000);
+		logger.info("Manager started...");
 	}
 }
