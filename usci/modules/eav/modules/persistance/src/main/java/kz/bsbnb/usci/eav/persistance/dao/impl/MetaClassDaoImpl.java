79d244cd46d903ab2c04ee232504c3b9ea47ebf7
Kanat Tulbassiyev <kanat.tulbassiyev@bsbnb.kz>
2016-05-25 09:38:03 +0600

    RefRepository implemented

--- usci/modules/eav/modules/persistance/src/main/java/kz/bsbnb/usci/eav/persistance/dao/impl/MetaClassDaoImpl.java
+++ usci/modules/eav/modules/persistance/src/main/java/kz/bsbnb/usci/eav/persistance/dao/impl/MetaClassDaoImpl.java
@@ -647,7 +647,7 @@ public class MetaClassDaoImpl extends JDBCSupport implements IMetaClassDao {
                 .where(EAV_M_COMPLEX_ATTRIBUTES.CONTAINING_ID.eq(meta.getId()))
                 .and(EAV_M_COMPLEX_ATTRIBUTES.CONTAINER_TYPE.eq(meta.getType()));
 
-        long t = System.currentTimeMillis();;
+        long t = System.currentTimeMillis();
 
         logger.debug(select.toString());
 
@@ -944,5 +944,125 @@ public class MetaClassDaoImpl extends JDBCSupport implements IMetaClassDao {
         }
 
         return metaClassNameList;
+    }
+
+    @Override
+    public List<Map<String, Object>> getSimpleResult(long metaId) {
+        Select select = getSimpleSelect(metaId);
+        return queryForListWithStats(select.getSQL(), select.getBindValues().toArray());
+    }
+
+    @SuppressWarnings("all")
+    @Override
+    public Select getSimpleSelect(long metaId) {
+        final String ebe = "ebe";
+        int counter = 1;
+
+        List<Map> refList = new LinkedList<>();
+
+        MetaClass metaClass = load(metaId);
+        final String metaClassName = metaClass.getClassName().toUpperCase();
+
+        SelectSelectStep select = context.select(EAV_BE_ENTITIES.as(ebe).ID.as(metaClassName + "_ID"));
+
+        TreeSet<String> names = new TreeSet<>();
+
+        for (String attribute : metaClass.getAttributeNames()) {
+            names.add(attribute);
+        }
+
+        for (String attribute : names) {
+            IMetaAttribute metaAttribute = metaClass.getMetaAttribute(attribute);
+            IMetaType metaType = metaAttribute.getMetaType();
+
+            if (metaType.isSet())
+                continue;
+
+            if (metaType.isComplex()) {
+                select.select(EAV_BE_COMPLEX_VALUES.as("cv" + counter).ENTITY_VALUE_ID
+                        .as(metaAttribute.getName().toUpperCase() + "_ID"));
+            } else {
+                MetaValue metaValue = (MetaValue) metaType;
+                switch (metaValue.getTypeCode()) {
+                    case STRING:
+                        select.select(EAV_BE_STRING_VALUES.as("sv" + counter).VALUE
+                                .as(metaAttribute.getName().toUpperCase()));
+                        break;
+                    case INTEGER:
+                        select.select(EAV_BE_INTEGER_VALUES.as("iv" + counter).VALUE
+                                .as(metaAttribute.getName().toUpperCase()));
+                        break;
+                    case BOOLEAN:
+                        select.select(EAV_BE_BOOLEAN_VALUES.as("bv" + counter).VALUE
+                                .as(metaAttribute.getName().toUpperCase()));
+                        break;
+                    case DOUBLE:
+                        select.select(EAV_BE_DOUBLE_VALUES.as("dov" + counter).VALUE
+                                .as(metaAttribute.getName().toUpperCase()));
+                        break;
+                    case DATE:
+                        select.select(EAV_BE_DATE_VALUES.as("dav" + counter).VALUE
+                                .as(metaAttribute.getName().toUpperCase()));
+                        break;
+                    default:
+                        throw new IllegalArgumentException(Errors.compose(Errors.E127));
+                }
+            }
+
+            counter++;
+        }
+
+        SelectJoinStep selectJoinStep = select.from(EAV_BE_ENTITIES.as(ebe));
+
+        counter = 1;
+        for (String attribute : names) {
+            IMetaAttribute metaAttribute = metaClass.getMetaAttribute(attribute);
+            IMetaType metaType = metaAttribute.getMetaType();
+
+            if (metaType.isSet())
+                continue;
+
+            if (metaType.isComplex()) {
+                selectJoinStep.leftOuterJoin(EAV_BE_COMPLEX_VALUES.as("cv" + counter))
+                        .on(EAV_BE_COMPLEX_VALUES.as("cv" + counter).ENTITY_ID.eq(EAV_BE_ENTITIES.as(ebe).ID))
+                        .and(EAV_BE_COMPLEX_VALUES.as("cv" + counter).ATTRIBUTE_ID.eq(metaAttribute.getId()));
+            } else {
+                MetaValue metaValue = (MetaValue) metaType;
+                switch (metaValue.getTypeCode()) {
+                    case STRING:
+                        selectJoinStep.leftOuterJoin(EAV_BE_STRING_VALUES.as("sv" + counter))
+                                .on(EAV_BE_STRING_VALUES.as("sv" + counter).ENTITY_ID.eq(EAV_BE_ENTITIES.as(ebe).ID))
+                                .and(EAV_BE_STRING_VALUES.as("sv" + counter).ATTRIBUTE_ID.eq(metaAttribute.getId()));
+                        break;
+                    case INTEGER:
+                        selectJoinStep.leftOuterJoin(EAV_BE_INTEGER_VALUES.as("iv" + counter))
+                                .on(EAV_BE_INTEGER_VALUES.as("iv" + counter).ENTITY_ID.eq(EAV_BE_ENTITIES.as(ebe).ID))
+                                .and(EAV_BE_INTEGER_VALUES.as("iv" + counter).ATTRIBUTE_ID.eq(metaAttribute.getId()));
+                        break;
+                    case BOOLEAN:
+                        selectJoinStep.leftOuterJoin(EAV_BE_BOOLEAN_VALUES.as("bv" + counter))
+                                .on(EAV_BE_BOOLEAN_VALUES.as("bv" + counter).ENTITY_ID.eq(EAV_BE_ENTITIES.as(ebe).ID))
+                                .and(EAV_BE_BOOLEAN_VALUES.as("bv" + counter).ATTRIBUTE_ID.eq(metaAttribute.getId()));
+                        break;
+                    case DOUBLE:
+                        selectJoinStep.leftOuterJoin(EAV_BE_DOUBLE_VALUES.as("dov" + counter))
+                                .on(EAV_BE_DOUBLE_VALUES.as("dov" + counter).ENTITY_ID.eq(EAV_BE_ENTITIES.as(ebe).ID))
+                                .and(EAV_BE_DOUBLE_VALUES.as("dov" + counter).ATTRIBUTE_ID.eq(metaAttribute.getId()));
+                        break;
+                    case DATE:
+                        selectJoinStep.leftOuterJoin(EAV_BE_DATE_VALUES.as("dav" + counter))
+                                .on(EAV_BE_DATE_VALUES.as("dav" + counter).ENTITY_ID.eq(EAV_BE_ENTITIES.as(ebe).ID))
+                                .and(EAV_BE_DATE_VALUES.as("dav" + counter).ATTRIBUTE_ID.eq(metaAttribute.getId()));
+                        break;
+                    default:
+                        throw new IllegalArgumentException(Errors.compose(Errors.E127));
+                }
+            }
+            counter++;
+        }
+
+        selectJoinStep.where(EAV_BE_ENTITIES.as(ebe).CLASS_ID.eq(metaId));
+
+        return selectJoinStep;
     }
 }
