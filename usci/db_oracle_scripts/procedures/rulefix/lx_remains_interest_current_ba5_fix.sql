97c091195f7453aa0d70341213a037cacf62d563
bauyrzhan.makhambetov <bauyrzhan.makhambetov@bsnbn.kz>
2016-07-14 14:41:47 +0600

    lx remains interest current ba5 write to log

--- usci/db_oracle_scripts/procedures/rulefix/lx_remains_interest_current_ba5_fix.sql
+++ usci/db_oracle_scripts/procedures/rulefix/lx_remains_interest_current_ba5_fix.sql
@@ -5,8 +5,10 @@ BEGIN
    select count(1) into v_count from lx_rinterest_current_ba5;
 
    if v_count > 0 THEN
-      dbms_output.put_line('rule remains_interest_current_ba5: ' || v_count || ' credits failed')
+      lx_write_log(sysdate, 'remains_interest_current_ba5', 'rule remains_interest_current_ba5: ' || v_count || ' credits failed');
+      dbms_output.put_line('rule remains_interest_current_ba5: ' || v_count || ' credits failed');
    else
+      lx_write_log(sysdate, 'remains_interest_current_ba5', 'rule lx_remains_interest_current_ba5 is ok !!!');
       dbms_output.put_line('rule lx_remains_interest_current_ba5 is ok !!!');
    end if;
 
