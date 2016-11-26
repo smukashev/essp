eead230fa185bed3ccd15a35bcf2371a65c45e0b
Бауыржан Махамбетов <baurmatic@gmail.com>
2016-11-27 00:01:16 +0600

    oracle db 12 migration: prod ips change

--- usci/modules/cli/src/main/java/kz/bsbnb/usci/cli/app/CLI.java
+++ usci/modules/cli/src/main/java/kz/bsbnb/usci/cli/app/CLI.java
@@ -2415,7 +2415,7 @@ public class CLI {
                     try {
                         try {
                             if (conn == null || conn.isClosed())
-                                conn = connectToDB("jdbc:oracle:thin:@10.8.1.200:1521:ESSP", "CORE", "core");
+                                conn = connectToDB("jdbc:oracle:thin:@10.8.2.200:1521:ESSP", "CORE", "core");
                         } catch (Exception e) {
                             System.out.println("Can't connect to DB: " + e.getMessage());
                             return;
