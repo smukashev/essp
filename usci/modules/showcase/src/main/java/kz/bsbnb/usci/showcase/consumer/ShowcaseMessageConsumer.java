68c01bd6393f791c2e78216fe5301f66d8cc34e1
Baurzhan Makhambetov <baurmatic@gmail.com>
2017-03-13 13:21:34 +0600

    E288 bugfix: extra sout removed

--- usci/modules/showcase/src/main/java/kz/bsbnb/usci/showcase/consumer/ShowcaseMessageConsumer.java
+++ usci/modules/showcase/src/main/java/kz/bsbnb/usci/showcase/consumer/ShowcaseMessageConsumer.java
@@ -100,7 +100,7 @@ public class ShowcaseMessageConsumer implements MessageListener {
                     if (entityFound)
                         Thread.sleep(50);
 
-                    if (entityFound && (failCounter %  1000) == 0) {
+                    if (entityFound && (failCounter %  1000) == 0 && failCounter > 0) {
                         //throw new IllegalStateException(Errors.compose(Errors.E288));
                         System.out.println("failCounter = " + failCounter);
                     }
