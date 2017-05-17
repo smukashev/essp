2bfd8c093c057e6ec85042d667012d9b447b138a
baurmatic <baurmatic@gmail.com>
2017-05-17 12:04:41 +0600

    trg added to prevent docno delete

---
+++ usci/db_oracle_objects/triggers/trg_docno_scanner.sql
@@ -0,0 +1,10 @@
+create or replace trigger trg_docno_scanner
+before DELETE
+  on eav_be_string_values for each row
+DECLARE
+  v_control number;
+BEGIN
+  if(:OLD.attribute_id = 14)
+    raise_application_error(-20002,'Нельзая удалить номер документа');
+  end if;
+end;
