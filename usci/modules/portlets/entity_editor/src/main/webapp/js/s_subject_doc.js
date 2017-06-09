ac846ae35989340ea2c7399f3050515483c58622
Assylkhan.Doskulov <asseke1993@gmail.com>
2017-06-09 16:07:55 +0600

    EntityPortlet: alert message changed

--- usci/modules/portlets/entity_editor/src/main/webapp/js/s_subject_doc.js
+++ usci/modules/portlets/entity_editor/src/main/webapp/js/s_subject_doc.js
@@ -232,7 +232,7 @@ Ext.onReady(function() {
                         Ext.MessageBox.alert(label_ERROR, label_ERROR_NO_DATA_FOR.format(error));
                     }
                     if(records && records.length == 0)
-                        Ext.MessageBox.alert(label_INFO, 'Необходимо указать хотя бы один идентификационный документ');
+                        Ext.MessageBox.alert(label_INFO, 'Поиск вернул 0 результатов');
 
                     Ext.getCmp('entityEditorShowBtn').enable();
                 }});
