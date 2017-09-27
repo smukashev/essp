f7b6ce578b5ecd9a7d4daf0a726b4a150266bd7a
Assylkhan.Doskulov <asseke1993@gmail.com>
2017-09-27 11:59:52 +0600

    add some locale libraries...

---
+++ usci/libs/exec.bat
@@ -0,0 +1,3 @@
+mvn install:install-file -Dfile=ojdbc6.jar -DgroupId=com.oracle -DartifactId=ojdbc6 -Dversion=11.2.0.4 -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=ddlutils-1.1.jar -DgroupId=kz.bsbnb -DartifactId=ddlutils -Dversion=1.1 -Dpackaging=jar -DgeneratePom=true
+
